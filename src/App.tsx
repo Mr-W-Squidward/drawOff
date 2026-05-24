1f895bc7e18412b73ebaa9e6e6c7541b3d4796f3
Wajeeh Alam <alamwajeeh@gmail.com>
2026-05-24 15:49:48 -0400

    left side canvas refs + basic drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useRef, useState, useEffect } from "react";
+
 const wordList = ["Bunny", "Sunflower", "Lavender Roses", "Glasses", "Sudoku",
     "Birthday Cake", "Bear", "Bird", "Sunset and Mountain Landscape", "Galaxy", "Cutie",
 ];
@@ -7,7 +9,39 @@ function chooseRandomWord(words: string[]): string {
   return words[randomNumber];
 }
 
-function App() {
+export default function App() {
+  const canvasLeftRef = useRef<HTMLCanvasElement>(null);
+  const canvasRightRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+  const canvas = canvasLeftRef.current;
+  if (!canvas) throw new Error('Could not load canvas')
+  const ctx = canvas.getContext('2d');
+
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+
+  const rect = canvas.getBoundingClientRect();
+
+  const isDrawing = { current: false };
+
+  window.addEventListener('mousedown', (event: MouseEvent) => {
+    isDrawing.current = true;
+    ctx?.beginPath();
+    ctx?.moveTo(event.clientX - rect.left, event.clientY - rect.top)
+  });
+  
+
+  window.addEventListener('mousemove', (event: MouseEvent) => {
+    if (!isDrawing.current) return;
+    ctx?.lineTo(event.clientX - rect.left, event.clientY - rect.top)
+    ctx?.stroke();
+  });
+
+  window.addEventListener('mouseup', (event: MouseEvent) => isDrawing.current = false );
+  window.addEventListener('mouseleave', (event: MouseEvent) => isDrawing.current = false );
+}, [])
+
   return (
     <>
       <div className="w-full h-screen bg-[#000000]">        
@@ -17,7 +51,7 @@ function App() {
           {/* Toolbar -- Shared Spot */}
         <header className="absolute inset-x-0 top-4 z-20">
           {/* Saved Colours */}
-          <div className="flex mx-auto items-center justify-center gap-2">
+          <div className="flex mx-auto items-center justify-center gap-2 select-none pointer-events-auto">
             <button className="w-10 h-10 bg-[#123412] border border-black"></button>
             <button className="w-10 h-10 bg-[#432234] border border-black"></button>
             <button className="w-10 h-10 bg-[#543543] border border-black"></button>
@@ -32,7 +66,7 @@ function App() {
         </header>
 
           {/* Drawing Prompt */}
-          <h1 className="absolute inset-x-0 text-center top-1/2 z-10">
+          <h1 className="absolute inset-x-0 text-center top-1/2 z-10 select-none pointer-events-auto">
             <span className="inline-block border border-black px-3 py-2 bg-[#345421]">
               {chooseRandomWord(wordList)}
             </span>
@@ -41,18 +75,12 @@ function App() {
           {/* Canvas Wrapper */}
           <div className="flex h-full">
 
-            <canvas className="w-1/2 bg-[#123123]">
+            <canvas id="canvasLeft" className="w-1/2 bg-[#123123]" ref={canvasLeftRef}/>
+            <canvas id="canvasRight" className="w-1/2 bg-[#521312]" ref={canvasRightRef}/>
 
-            </canvas>
-
-            <canvas className="w-1/2 bg-[#321321]">
-
-            </canvas>
           </div>
         </div>
       </div>
     </>
   )
 }
-
-export default App
