12c2ba06afa4aecda44a829b19ea6428e8d922f7
Wajeeh Alam <alamwajeeh@gmail.com>
2026-06-03 21:40:39 -0400

    ui + linewidth

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
 const wordList = ["Bunny", "Sunflower", "Lavender Roses", "Glasses", "Sudoku",
-    "Birthday Cake", "Bear", "Bird", "Sunset and Mountain Landscape", "Galaxy", "Cutie",
+    "Birthday Cake", "Bear", "Bird", "Sunset and Mountain Landscape", "Galaxy", "Cutie", "Tung Tung Tung Sahur"
 ];
 
 function chooseRandomWord(words: string[]): string {
@@ -11,6 +11,7 @@ function chooseRandomWord(words: string[]): string {
 
 export default function App() {
   const brushColourRef = useRef("black");
+  const lineWidthRef = useRef(4);
   const canvasLeftRef = useRef<HTMLCanvasElement>(null);
   const canvasRightRef = useRef<HTMLCanvasElement>(null);
 
@@ -19,6 +20,7 @@ export default function App() {
   }
 
   // Canvas drawing refs -> add line width / colour wheel / socket.io later
+  const [selectedWord] = useState(() => chooseRandomWord(wordList));
   useEffect(() => {
     const setupCanvas = (canvas: HTMLCanvasElement | null) => {
       if (!canvas) throw new Error('Could not load canvas')
@@ -39,6 +41,7 @@ export default function App() {
         isDrawing.current = true;
         if (!ctx) return;
         ctx.strokeStyle = brushColourRef.current;
+        ctx.lineWidth = lineWidthRef.current;
         ctx.beginPath();
         ctx.moveTo(event.clientX - rect.left, event.clientY - rect.top);
       };
@@ -84,26 +87,41 @@ export default function App() {
         <div className="relative w-full h-screen">
 
           {/* Toolbar -- Shared Spot */}
-        <header className="absolute inset-x-0 top-4 z-20">
-          {/* Saved Colours */}
-          <div className="flex mx-auto items-center justify-center gap-2 select-none pointer-events-auto">
-            <button className="w-10 h-10 bg-[#000000] border border-black" onClick={() => changeColour("#000000")}/>
-            <button className="w-10 h-10 bg-[#FF0000] border border-black" onClick={() => changeColour("#FF0000")}></button>
-            <button className="w-10 h-10 bg-[#0000FF] border border-black" onClick={() => changeColour("#0000FF")}></button>
-            <button className="w-10 h-10 bg-[#00FF00] border border-black" onClick={() => changeColour("#00FF00")}></button>
-            <button className="w-10 h-10 bg-[#FFFF00] border border-black" onClick={() => changeColour("#FFFF00")}></button>
-          </div>
+          <header className="absolute inset-x-0 top-4 z-20">
+            <div className="flex justify-center">
+              {/* Saved Colours */}
+              <div className="flex items-center justify-center gap-2 select-none pointer-events-auto">
+                <button className="w-10 h-10 bg-[#000000] border border-black" onClick={() => changeColour("#000000")}/>
+                <button className="w-10 h-10 bg-[#FF0000] border border-black" onClick={() => changeColour("#FF0000")}></button>
+                <button className="w-10 h-10 bg-[#0000FF] border border-black" onClick={() => changeColour("#0000FF")}></button>
+                <button className="w-10 h-10 bg-[#00FF00] border border-black" onClick={() => changeColour("#00FF00")}></button>
+                <button className="w-10 h-10 bg-[#FFFF00] border border-black" onClick={() => changeColour("#FFFF00")}></button>
+              </div>
 
-          {/* Colour Wheel */}
-          <div>
-            
-          </div>
-        </header>
+              {/* Line Width Slider */}
+              <input
+                type="range"
+                min="1"
+                max="16"
+                step="3"
+                defaultValue={lineWidthRef.current}
+                onChange={(e) => {
+                  const v = Number((e.target as HTMLInputElement).value);
+                  lineWidthRef.current = v;
+                }}
+              />
+
+              {/* Colour Wheel */}
+              <div>
+                
+              </div>
+            </div>
+          </header>
 
           {/* Drawing Prompt */}
           <h1 className="absolute inset-x-0 text-center top-1/2 z-10 select-none pointer-events-none">
             <span className="inline-block border border-black px-3 py-2 bg-[#345421]">
-              {chooseRandomWord(wordList)}
+              {selectedWord}
             </span>
           </h1>
 
